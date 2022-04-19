aba5579d87a479fb455636d84b9dfa07e7e6c7ac
kobyunggwon <zox5702@naver.com>
2022-04-19 15:58:36 +0900

    비즈맵 모바일 테스트

--- src/main/java/com/bizmap/controller/BizmapController.java
+++ src/main/java/com/bizmap/controller/BizmapController.java
@@ -3,9 +3,14 @@ package com.bizmap.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class BizmapController {
+    @GetMapping("/index_m")
+    public String login() {
+        return "bizmap/index_m";
+    }
 }
